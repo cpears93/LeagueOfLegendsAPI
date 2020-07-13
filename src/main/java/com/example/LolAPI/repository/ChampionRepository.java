5b85b03b330fd451f817aeb534af27cf467f79a4
Christopher Pearson <cpearson3140@gmail.com>
2020-07-13 13:22:33 +0100

    Populated ChampionRepository and GameModeRepository files
    Signed-off-by: Christopher Pearson <cpearson3140@gmail.com>

--- src/main/java/com/example/LolAPI/repository/ChampionRepository.java
+++ src/main/java/com/example/LolAPI/repository/ChampionRepository.java
@@ -1,4 +1,20 @@
 package com.example.LolAPI.repository;
 
-public interface ChampionRepository {
+import com.example.LolAPI.models.Champion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ChampionRepository extends JpaRepository<Champion, Long> {
+
+    List<Champion> findByName(String name);
+    List<Champion> findByEpithet(String epithet);
+    List<Champion> findByRoles(String roles);
+    List<Champion> findByAbilities(String abilities);
+    List<Champion> findByMana(String mana);
+    List<Champion> findByArmour(String armour);
+    List<Champion> findByAttackSpeed(String attackSpeed);
+    List<Champion> findByAttackDamage(String attackDamage);
 }
--- src/main/java/com/example/LolAPI/repository/GameModeRepository.java
+++ src/main/java/com/example/LolAPI/repository/GameModeRepository.java
@@ -1,4 +1,14 @@
 package com.example.LolAPI.repository;
 
-public interface GameModeRepository {
+import com.example.LolAPI.models.GameMode;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GameModeRepository extends JpaRepository<GameMode, Long> {
+
+    List<GameMode> findByRegulartMode(String regularMode);
+    List<GameMode> findByFeaturedGameMode(String featuredGameMode);
 }
