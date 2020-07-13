5679d9b9d7e83d0c52b93729e1c6864228e72b79
Christopher Pearson <cpearson3140@gmail.com>
2020-07-13 12:46:33 +0100

    created Game Mode files
    Signed-off-by: Christopher Pearson <cpearson3140@gmail.com>

--- src/main/java/com/example/LolAPI/components/DataLoader.java
+++ src/main/java/com/example/LolAPI/components/DataLoader.java
@@ -1,4 +1,22 @@
 package com.example.LolAPI.components;
 
-public class DataLoader {
+import com.example.LolAPI.repository.ChampionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    ChampionRepository championRepository;
+
+    public DataLoader() {
+
+    }
+
+    public void run(ApplicationArguments args) {
+
+    }
 }
