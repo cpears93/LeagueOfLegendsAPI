62a6e54a5c9b05df164fb290a6860ede65c70baa
Christopher Pearson <cpearson3140@gmail.com>
2020-07-13 13:45:55 +0100

    Populated GameModecontroller.java file
    Signed-off-by: Christopher Pearson <cpearson3140@gmail.com>

--- src/main/java/com/example/LolAPI/controller/GameModeController.java
+++ src/main/java/com/example/LolAPI/controller/GameModeController.java
@@ -1,4 +1,46 @@
 package com.example.LolAPI.controller;
 
+import com.example.LolAPI.models.GameMode;
+import com.example.LolAPI.repository.GameModeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class GameModeController {
+
+    @Autowired
+    GameModeRepository gameModeRepository;
+
+    @GetMapping(value = "/gameModes/{id}")
+    public ResponseEntity getGameMode(@PathVariable Long id){
+        return new ResponseEntity<>(gameModeRepository.findById(id), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/gameModes")
+    public ResponseEntity <List<GameMode>> getAllGameModes(){
+        return new ResponseEntity<>(gameModeRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/gameModes")
+    public ResponseEntity<GameMode> createGameMode(@RequestBody GameMode gameMode){
+        gameModeRepository.save(gameMode);
+        return new ResponseEntity<>(gameMode, HttpStatus.CREATED);
+    }
+
+    @PatchMapping(value = "/gameModes/{id}")
+    public ResponseEntity<GameMode> updateGameMode(@RequestBody GameMode gameMode){
+        gameModeRepository.save(gameMode);
+        return new ResponseEntity<>(gameMode, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/gameModes/{id}")
+    public ResponseEntity<GameMode> deleteGameMode(@PathVariable Long id){
+        GameMode found = gameModeRepository.getOne(id);
+        gameModeRepository.delete(found);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
 }
